8fa64e9ec33e65bef9c6feb7694fa50158cab4de
João Vicente <joao@javabeats.com.br>
2024-09-13 16:16:46 -0300

    feat: add cors and error handling

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import express, {Request, Response, NextFunction} from 'express';
 import morgan from 'morgan';
 import { mintAndTransfer } from './Web3Provider';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -10,12 +11,19 @@ const PORT: number = parseInt(`${process.env.PORT || 3001}`);
 
 app.use(morgan("tiny"));
 
+console.log(process.env.CORS_ORIGIN_URL);
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN_URL || '*'
+}));
+
 app.post("/mint/:wallet", async (req: Request, res: Response, next: NextFunction) => {
     try {
         const tx = await mintAndTransfer(req.params.wallet);
         res.json(tx);
     } catch (error: any) {
-        res.status(500).json(error.message);
+        console.log(error);
+        res.status(500).json(error.cause ? error.cause.message : error.message);
     }
 });
 
